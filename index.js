6eb02d523f36c0b3711b30de0694ea26d5861137
Daniel Interiano <dannyboyint@gmail.com>
2021-07-21 15:57:07 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callBack){
+     callBack();
+}
+
+function returnsANamedFunction() {
+   return function callBack(){}
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){}
+    }
